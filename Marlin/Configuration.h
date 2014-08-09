d75436fca76f60e7f7b7ab0e1b60a66f1440b090
Hampus Sandberg <hampus.sandberg@gmail.com>
2014-08-09 13:30:56 +0200

    Small changes to work with E3D-v6 hot end

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -158,10 +158,10 @@
 // When temperature exceeds max temp, your heater will be switched off.
 // This feature exists to protect your hotend from overheating accidentally, but *NOT* from thermistor short/failure!
 // You should use MINTEMP for thermistor short/failure protection.
-#define HEATER_0_MAXTEMP 275
+#define HEATER_0_MAXTEMP 290
 #define HEATER_1_MAXTEMP 275
 #define HEATER_2_MAXTEMP 275
-#define BED_MAXTEMP 150
+#define BED_MAXTEMP 130
 
 // If your bed has low resistance e.g. .6 ohm and throws the fuse you can duty cycle it to reduce the
 // average current. The value should be an integer and the heat bed will be turned on for 1 interval of
@@ -443,7 +443,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 // default settings
 
 #define PORTABEE_EXTRUDER_STEPS    (2056)
-#define WADE_EXTRUDER_STEPS        (1550)
+#define WADE_EXTRUDER_STEPS        (1500)
 #define CALIBRATED_X_STEPS         (158.19)  // 39.45
 #define CALIBRATED_Y_STEPS         (160.36)  // 39.89
 #define CALIBRATED_Z_STEPS         (6456.48)  // 1600
